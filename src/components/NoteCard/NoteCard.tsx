b37fcc4d4242387ff7d0bb235b474984b7018192
SAMER\samer <samercherif@hotmail.fr>
2024-11-23 22:53:39 +0000

    refactor notes list to have a mansory layout with alternate colors for the notes

--- src/components/NoteCard/NoteCard.tsx
+++ src/components/NoteCard/NoteCard.tsx
@@ -7,10 +7,21 @@ interface NoteCardProps {
 }
 
 const NoteCard = ({ note, onClick }: NoteCardProps) => {
-  const previewContent = note.body.length > 100 ? `${note.body.slice(0, 100)}...` : note.body
+  const previewContent = note.body.length > 300 ? `${note.body.slice(0, 300)}...` : note.body
 
   return (
-    <div data-testid={'note-card'} onClick={() => onClick(note)} className={'note-card'}>
+    <div
+      data-testid={'note-card'}
+      onClick={() => onClick(note)}
+      className={'note-card'}
+      role={'button'}
+      tabIndex={0}
+      onKeyDown={(e) => {
+        if (e.key === 'Enter' || e.key === ' ') {
+          onClick(note)
+        }
+      }}
+    >
       <p className={'note-card-content'}>{previewContent}</p>
     </div>
   )
