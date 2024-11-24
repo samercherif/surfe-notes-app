ed81add20666ff830eb41926944ca137f8169c62
SAMER\samer <samercherif@hotmail.fr>
2024-11-24 22:02:28 +0000

    use HashRouter for github pages compatibility
    replaced BrowserRouter with HashRouter to handle client-side routing on github Pages
    this ensures proper handling of direct URL access and page refreshes

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,17 +1,15 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { HashRouter, Route, Routes } from 'react-router-dom'
 import NotesList from '@src/pages/NotesList/NotesList'
 import NoteEditor from '@pages/NoteEditor/NoteEditor'
-import { BASE_URL, DEV } from '@src/constants'
 
 const App = () => {
-  const basename = DEV ? '/' : BASE_URL
   return (
-    <BrowserRouter basename={basename}>
+    <HashRouter>
       <Routes>
         <Route path={'/'} element={<NotesList />} />
         <Route path={'/notes/:id'} element={<NoteEditor />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
